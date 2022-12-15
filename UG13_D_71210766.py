bfdc72b6662c5e00362d2d7c84b06d7ed005b2b9
71210766 <93719122+71210766@users.noreply.github.com>
2022-12-15 15:40:14 +0700

    Add files via upload

---
+++ UG13_D_71210766.py
@@ -0,0 +1,24 @@
+import turtle
+an=turtle.Screen()
+a=turtle.Turtle()
+a.shape("turtle")
+a.color("green")
+an.bgcolor("blue")
+a.right(90)
+a.pensize(10)
+a.bk(130)
+a.fd(130)
+a.bk(0)
+a.left(90)
+a.circle(40,180)
+a.left(90)
+a.bk(0)
+a.left(90)
+a.circle(25,180)
+a.down(180)
+a.penup()
+a.fd(80)
+a.pendown()
+
+
+
